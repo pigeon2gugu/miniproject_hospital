0de21fee3535807adc25ad988b6db260da25e40d
pigeon2gugu <khn1135@gmail.com>
2022-10-19 14:26:51 +0900

    java와 MySQL DB연동 test springboot 연동

--- src/main/java/com/line/dao/UserDaoFactory.java
+++ src/main/java/com/line/dao/UserDaoFactory.java
@@ -1,13 +1,20 @@
 package com.line.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     //조립 담당
+
+    @Bean
     public UserDao awsUserDao() {
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
     }
 
+    @Bean
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -4,10 +4,35 @@ import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    @DisplayName("add and select doing well")
+
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        User user = new User("8", "EternityHwan", "123");
+        //userDao.add(user);
+
+        User selectedUser = userDao.select("8");
+        Assertions.assertEquals("EternityHwan", selectedUser.getName());
+        System.out.println(selectedUser.getName());
+    }
+}
+
 
     /*
     @Test
@@ -20,12 +45,6 @@ class UserDaoTest {
         User selectedUser = userDao.select("8");
         Assertions.assertEquals("EternityHwan", selectedUser.getName());
 
-
-     */
-    @Test
-    @DisplayName("add and select doing well")
-
-    /*
     Factory 적용 전
     void addAndSelect() throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
@@ -39,13 +58,3 @@ class UserDaoTest {
 
     }
      */
-    void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("8", "EternityHwan", "123");
-        //userDao.add(user);
-
-        User selectedUser = userDao.select("8");
-        Assertions.assertEquals("EternityHwan", selectedUser.getName());
-        System.out.println(selectedUser.getName());
-    }
-}
